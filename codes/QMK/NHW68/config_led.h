2a857ab45c796d5583d6c083efa7fabe36d6bf68
Yifei Ding <dingyifeiair@gmail.com>
2020-01-27 17:46:07 -0800

    creating the info.json and starting my configuration journey

---
+++ codes/QMK/NHW68/config_led.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#ifdef RGB_MATRIX_ENABLE
+    #define DRIVER_ADDR_1 0b1010000
+    #define DRIVER_ADDR_2 0b1011100
+    #define DRIVER_COUNT 2
+    #define DRIVER_1_LED_TOTAL 41
+    #define DRIVER_2_LED_TOTAL 27
+    #define DRIVER_LED_TOTAL 68
+#endif
